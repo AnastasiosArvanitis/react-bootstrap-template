4bb7443abdbb8376a2ad3b784c1795c0df5abd80
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-10-21 11:50:52 +0200

    Setting up the template

---
+++ src/components/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = () => {
+    return(
+        <div>
+            <h1>React bootstrap Template !!!!!!!</h1>
+            <p>This is a ready setup for React.js apps with Bootstrap :) 
+            </p>
+        </div>
+    );
+}
+
+export default Test;
