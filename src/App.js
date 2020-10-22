b10cb48767457b35806597a16cfa0801f61f19d8
Anastasios Arvanitis <62649615+AnastasiosArvanitis@users.noreply.github.com>
2020-10-22 15:52:14 +0200

    Updating the routes in the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import NavBar from './components/NavBar.jsx';
-import Home from './components/Home.jsx';
-import User from './components/User.jsx';
+import NavBarContent from './content/NavBarContent.js';
+import NavBar from './components/NavBar/NavBar.jsx';
+import Home from './components/Home/Home.jsx';
+import Documentation from './components/Documentation/Documentation.jsx';
+import CustomComponents from './components/CustomComponents/CustomComponents.jsx';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -13,8 +15,8 @@ const App = () => {
         <NavBar/>
         <Switch>
             <Route path='/' exact component={Home} />
-            <Route path='/User' exact component={User} />
-        <Home/>
+            <Route path='/Documentation' component={Documentation} />
+            <Route path='/CustomComponents' component={CustomComponents} />
         </Switch>
         </>
     );
